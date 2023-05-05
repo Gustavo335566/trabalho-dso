5d006ffd08599afb439de41991efd29baf9fd118
Gustavo335566 <bernardesguh@gmail.com>
2023-05-05 14:21:06 -0300

    add, excluir, alterar, imprimir, colocados

--- controle/controlador_agenda.py
+++ controle/controlador_agenda.py
@@ -6,3 +6,29 @@ class Controlador_Agenda(Agenda, Tela_Agenda):
         self.__controlador_principal = controlador_principal
         self.__tela_agenda = Tela_Agenda()
 
+    def ver_agenda(self):
+        mostra = self.minhas_consultas
+        Tela_Agenda().imprimir(mostra)
+
+    def inclui_consulta(self, consulta: Consulta):
+        if isinstance(consulta, Consulta):
+            for i in self.minhas_consultas:
+                if(consulta == i):
+                    return "Consulta duplicada"
+            self.minhas_consultas.append(consulta)
+            return "Consulta incluida"
+
+    def exclui_consulta(self, consulta: Consulta):
+        if isinstance(consulta, Consulta):
+            for i in self.minhas_consultas:
+                if(consulta == i):
+                    self.minhas_consultas.remove(consulta)
+                    return "Consulta excluida"
+            return "Consulta inexistente"
+
+    def Altera_consulta(self, consulta: Consulta):
+        if isinstance(consulta, Consulta):
+            for i in self.minhas_consultas:
+                if(consulta == i):
+                    return self.minhas_consultas[i]
+            return "Consulta inexistente"
